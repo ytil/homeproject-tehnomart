81d48ca5245122d15f1b8dc130b69fa190992306
Dmitry Ivannikov <ivannikov.web@gmail.com>
2019-03-11 16:47:27 +0300

    slider fix

--- app/js/main.js
+++ app/js/main.js
@@ -8,7 +8,7 @@ let closeButton = document.querySelector("#close-button");
 let openButton = document.querySelector("#open-button");
 
 [modalOverlay, closeButton, openButton].forEach(elem => {
-  elem.addEventListener('click', function () {
+  elem.addEventListener('click', function() {
     modal.classList.toggle("closed");
     modalOverlay.classList.toggle("closed");
     body.classList.toggle("overflow-hidden");
@@ -18,15 +18,33 @@ let openButton = document.querySelector("#open-button");
 
 //features-slider
 const firstSliderOptions = {
-  'sliderName': 'slider-1',
-  '2pauseOnHover': true,
+  'sliderClassName': '.slider-1',
+  'slideClassName': '.slide',
+  'pauseOnHover': true,
   'stopOnClick': true,
   'autoplayInterval': 5000,
+  'dots': true,
+  'dotsPanelClassName': '.control-panel',
+  'dotClassName': '.control-element',
   'arrows': true,
-  'prevButtonId': 'prev',
-  'nextButtonId': 'next',
-  'dots': true
+  'prevButtonClassName': '.prev',
+  'nextButtonClassName': '.next'
 };
+
+// const options = {
+//   'sliderClassName': '.slider-1',
+//   'slideClassName': '.slide',
+//   'pauseOnHover': true,
+//   'stopOnClick': true,
+//   'autoplayInterval': 5000,
+//   'dots': true,
+//   'dotsPanelClassName':'.control-panel',
+//   'dotClassName': '.dot',
+//   'arrows': true,
+//   'prevButtonClassName': '.prev',
+//   'nextButtonClassName': '.next'
+//   'slideDidChanged: 'function' //  callback when slide did changed
+// };
 
 
 let sliderFeatures = new Sl(firstSliderOptions);
@@ -35,11 +53,14 @@ sliderFeatures.start();
 
 //services-slider
 const secondSliderOptions = {
-  'sliderName': 'slider-2',
+  'sliderClassName': '.slider-2',
+  'slideClassName': '.slide',
   'pauseOnHover': true,
   'stopOnClick': true,
   'autoplayInterval': 5000,
-  'dots': true
+  'dots': true,
+  'dotsPanelClassName': '.control-panel',
+  'dotClassName': '.control-element',
 };
 
 let sliderServices = new Sl(secondSliderOptions);
